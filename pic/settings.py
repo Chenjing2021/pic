341af288e9f07bd65bb336eddf3099305822463e
Chenjing2021 <78714912+Chenjing2021@users.noreply.github.com>
2021-05-24 11:40:25 -0400

    Add files via upload

--- pic/settings.py
+++ pic/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ub#y1iny0i^uhaszh3^4-hxaut*gas0kikrn9$9o%=4_=7lk%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pic-1-demo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
